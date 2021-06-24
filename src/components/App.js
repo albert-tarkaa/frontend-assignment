1f7138a050f603376648adaeb745c1a9c846017f
albert.tarkaa <albert.tarkaa@tenece.com>
2021-06-24 10:20:40 +0100

    House keeping

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,16 @@ import Movies from './Movies';
 const App = () => {
   
   return (
-	  <MoviesContextPovider>
-        <main className='main-container'>
-          <Header />
-          <section className='description'>
-            <p>Most Recent Movies</p>
-          </section>
-          <section className='container'>
-				<Movies />
-          </section>
-			  </main>
+    <MoviesContextPovider>
+      <main className='main-container'>
+        <Header />
+        <section className='description'>
+          <p>Most Recent Movies</p>
+        </section>
+        <section className='container'>
+          <Movies />
+        </section>
+      </main>
     </MoviesContextPovider>
   );
 };
